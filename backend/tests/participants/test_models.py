1f045f2108af85c9c16a5b053f1638ef6c470856
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-03-07 14:49:03 -0300

    test: Adding tests for 'Participant' model

---
+++ backend/tests/participants/test_models.py
@@ -0,0 +1,33 @@
+import pytest
+from participants.models import Participant
+from django.db.utils import IntegrityError
+
+@pytest.fixture
+def participant():
+    return Participant.objects.create(
+        first_name='John',
+        last_name='Doe',
+        participation=50
+    )
+
+@pytest.mark.django_db
+class TestParticipantModel:
+    def test_participant_str_representation(self, participant):
+        assert str(participant) == 'John Doe'
+
+    def test_valid_data_creates_participant_instance(self):
+        participant = Participant.objects.create(
+            first_name='First',
+            last_name='Last',
+            participation=50
+        )
+
+        assert len(Participant.objects.all()) == 1
+
+    def test_participant_participation_non_negative(self):
+        with pytest.raises(IntegrityError):
+            Participant.objects.create(
+                first_name='Jane',
+                last_name='Doe',
+                participation=-50
+            )
