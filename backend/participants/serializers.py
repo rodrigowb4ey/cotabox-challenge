4e9e345d3db06a5b6a3a178e53581cdd8cf4dc53
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-03-07 14:49:54 -0300

    feat: Updating ParticipantSerializer with the field validations

--- backend/participants/serializers.py
+++ backend/participants/serializers.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from participants.models import Participant
@@ -6,9 +7,14 @@ from participants.models import Participant
 class ParticipantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Participant
-        fields = [
-            'id',
-            'first_name',
-            'last_name',
-            'participation'
-        ]
+        fields = '__all__'
+
+    def validate_first_name(self, value):
+        if not all(char.isalpha() or char == '-' for char in value):
+            raise ValidationError("First name should only contain alphabetic characters and hyphens")
+        return value
+
+    def validate_last_name(self, value):
+        if not all(char.isalpha() or char == '-' for char in value):
+            raise ValidationError("Last name should only contain alphabetic characters and hyphens")
+        return value
