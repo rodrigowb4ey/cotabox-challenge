2b478f27188df0ee9edb3753829e2662c9b8040c
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-03-08 13:21:10 -0300

    fix: Ensuring participation field value is greater than or equal to 1 + Trailing and leading spaces validation for name inputs

--- backend/participants/serializers.py
+++ backend/participants/serializers.py
@@ -10,15 +10,40 @@ class ParticipantSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate_first_name(self, value):
-        if not all(char.isalpha() or char == '-' for char in value):
+        if not value:
+            raise ValidationError('This field is required')
+        
+        if not all(char.isalpha() or char == '-' for char in value) or value != value.strip():
             raise ValidationError(
                 'First name should only contain alphabetic characters and hyphens'
             )
+        
+        if value.strip() != value:
+            raise ValidationError(
+                'First name cannot have leading or trailing spaces'
+            )
+
         return value
 
     def validate_last_name(self, value):
-        if not all(char.isalpha() or char == '-' for char in value):
+        if not value:
+            raise ValidationError('This field is required')
+        
+        if not all(char.isalpha() or char == '-' for char in value) or value != value.strip():
             raise ValidationError(
                 'Last name should only contain alphabetic characters and hyphens'
+            )
+        
+        if value.strip() != value:
+            raise ValidationError(
+                'Last name cannot have leading or trailing spaces'
+            )
+        
+        return value
+    
+    def validate_participation(self, value):
+        if value <= 0:
+            raise ValidationError(
+                'Ensure this value is greater than or equal to 1'
             )
         return value
