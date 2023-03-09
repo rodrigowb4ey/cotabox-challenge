9f7eece988e9a5ed8dd68a30b36d09a97d876801
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-03-09 12:47:39 -0300

    feat: Nav bar and modals for login and registering users

--- participation-form/src/App.vue
+++ participation-form/src/App.vue
@@ -1,8 +1,28 @@
 <template>
-  <header class="flex items-center justify-center bg-cyan-500">
+  <header class="flex gap-8 items-center justify-center bg-cyan-500">
     <DataForm @form-submitted="addParticipant"></DataForm>
+    <nav class="flex items-center justify-end px-4 py-2 bg-gray-800">
+      <button @click="showLoginModal = true" class="text-white text-sm mr-4">Login</button>
+      <button @click="showRegisterModal = true" class="text-white text-sm">Register</button>
+    </nav>
   </header>
   <main class="flex items-center justify-center align-middle flex-col">
+    <div v-if="showLoginModal">
+      <div class="modal-overlay" @click="showLoginModal = false"></div>
+      <div class="modal-container">
+        <div class="modal-content">
+          <LoginForm @login-form-closed="closeLoginModal"></LoginForm>
+        </div>
+      </div>
+    </div>
+    <div v-if="showRegisterModal">
+      <div class="modal-overlay" @click="showRegisterModal = false"></div>
+      <div class="modal-container">
+        <div class="modal-content">
+          <RegisterForm @register-form-closed="closeRegisterModal"></RegisterForm>
+        </div>
+      </div>
+    </div>
     <h1 class="mt-10 font-bold text-gray-600 text-2xl">PARTICIPATION DATA</h1>
     <h2 class="pt-5 text-[14px] text-gray-700 pb-10">Keeping track of the %.</h2>
     <section id="data-display" class="flex items-center justify-center gap-x-28 w-full">
@@ -25,6 +45,8 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Doughnut } from 'vue-chartjs'
 import DataTable from './components/DataTable.vue'
 import DataForm from './components/DataForm.vue'
+import LoginForm from './components/LoginForm.vue'
+import RegisterForm from './components/RegisterForm.vue'
 import axios from 'axios'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
@@ -35,10 +57,14 @@ export default {
   components: {
     Doughnut,
     DataTable,
-    DataForm
+    DataForm,
+    LoginForm,
+    RegisterForm
   },
   data() {
     return {
+      showLoginModal: false,
+      showRegisterModal: false,
       participants: [],
       chartKey: 0,
       chartData: {
@@ -80,6 +106,12 @@ export default {
     }
   },
   methods: {
+    closeRegisterModal() {
+      this.showRegisterModal = false
+    },
+    closeLoginModal() {
+      this.showLoginModal = false
+    },
     async removeParticipantFromList(id) {
       const index = this.participants.findIndex((participant) => participant.id == id)
 
@@ -94,7 +126,7 @@ export default {
         const response = await axios.post('http://localhost:8000/api/participants/', participant, {
           headers: {
             Authorization:
-              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc4MzIxNzc2LCJpYXQiOjE2NzgzMjE0NzYsImp0aSI6ImNiY2ZmODg3YTBlYzRlNzk5YTdlYjg0NDU0N2ZmZjIzIiwidXNlcl9pZCI6MX0.leE-nid98y72eMARjzJAkEOTljml-Hg46ufnQRo0aJU'
+              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc4MzIyMzU2LCJpYXQiOjE2NzgzMjIwNTYsImp0aSI6IjQ5ZmViZDVhOGMyNDQ5NTdiNzViNzdmNDM5ZGE4OWZmIiwidXNlcl9pZCI6MX0.O1oI6UHiQQ-6GGW3fdYsOt0ddP72Wu-6XPoaZkx1fBM'
           }
         })
 
@@ -146,3 +178,28 @@ export default {
   }
 }
 </script>
+
+<style>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+}
+
+.modal-container {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1000;
+}
+
+.modal-content {
+  background-color: #fff;
+  padding: 1rem;
+}
+</style>
