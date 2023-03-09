22a7e1ef9fd1376fe78610c2000708acadafdfc4
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-03-09 12:22:49 -0300

    feat: RegisterForm component

---
+++ participation-form/src/components/RegisterForm.vue
@@ -0,0 +1,123 @@
+<template>
+  <VeeForm @submit="register">
+    <section class="mb-4">
+      <label for="username" class="block text-gray-700 font-bold mb-2">Username:</label>
+      <Field
+        type="text"
+        id="username"
+        name="username"
+        v-model="registerForm.username"
+        placeholder="Enter your username"
+        class="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        :rules="validateUsername"
+      />
+      <ErrorMessage class="text-red-600" name="username"></ErrorMessage>
+    </section>
+    <section class="mb-4">
+      <label for="password" class="block text-gray-700 font-bold mb-2">Password:</label>
+      <Field
+        type="password"
+        id="password"
+        name="password"
+        v-model="registerForm.password1"
+        placeholder="Enter your password"
+        class="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        :rules="validatePassword"
+      />
+      <ErrorMessage class="text-red-600" name="password"></ErrorMessage>
+    </section>
+    <section class="mb-4">
+      <label for="confirm-password" class="block text-gray-700 font-bold mb-2"
+        >Confirm password:</label
+      >
+      <Field
+        type="password"
+        id="confirm-password"
+        name="confirm-password"
+        v-model="registerForm.password2"
+        placeholder="Confirm your password"
+        class="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        :rules="validatePassword"
+      />
+      <ErrorMessage class="text-red-600" name="password"></ErrorMessage>
+    </section>
+    <section class="flex items-center justify-between">
+      <button
+        type="submit"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Register
+      </button>
+    </section>
+  </VeeForm>
+</template>
+
+<script>
+import { Form as VeeForm, Field, ErrorMessage } from 'vee-validate'
+import axios from 'axios'
+
+export default {
+  name: 'LoginForm',
+  components: {
+    VeeForm,
+    Field,
+    ErrorMessage
+  },
+  data() {
+    return {
+      registerForm: {
+        username: '',
+        password1: '',
+        password2: ''
+      }
+    }
+  },
+  methods: {
+    async register() {
+      try {
+        const response = await axios.post(
+          'http://localhost:8000/api/auth/register/',
+          this.registerForm
+        )
+        if (response.status == 201) {
+          const accessToken = response.data['access_token']
+
+          localStorage.setItem('access_token', accessToken)
+          console.log(localStorage)
+          this.$emit('register-form-closed', true)
+          this.clearRegisterForm()
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    clearRegisterForm() {
+      this.registerForm.username = ''
+      this.registerForm.password1 = ''
+      this.registerForm.password2 = ''
+    },
+    validateUsername(value) {
+      if (!value) {
+        return 'This field is required'
+      }
+
+      if (value != value.trim()) {
+        return 'No leading or trailing spaces'
+      }
+
+      return true
+    },
+    validatePassword(value) {
+      if (!value) {
+        return 'This field is required'
+      }
+
+      if (value != value.trim()) {
+        return 'No leading or trailing spaces'
+      }
+
+      return true
+    }
+  }
+}
+</script>
